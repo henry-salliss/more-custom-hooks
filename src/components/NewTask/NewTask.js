d7017e716ea5bfe7e6a7691d29c0993bb679e7e0
Henry Salliss <henrysalliss@gmail.com>
2021-11-17 18:18:28 +0000

    made NewTask.js use the useHttp hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,62 +1,30 @@
-import { useState } from "react";
-// import useHttp from "../../hooks/use-http";
+import useHttp from "../../hooks/use-http";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const sendTask = (taskText, taskData) => {
+    const generatedId = taskData.name;
+    const createdTask = { id: generatedId, text: taskText };
 
-  // const taskFunction = (data) => {
-  //   const generatedId = data.name; // firebase-specific => "name" contains generated id
-  //   const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  };
 
-  //   props.onAddTask(createdTask);
-  // };
-
-  // const {
-  //   isLoading,
-  //   error,
-  //   sendRequest: enterTaskHandler,
-  // } = useHttp({
-  //   url: "https://custom-react-hooks-e706f-default-rtdb.firebaseio.com/tasks.json",
-  //   method: "POST",
-  //   body: JSON.stringify({ text: taskText }),
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // });
+  const { isLoading, error, sendRequest } = useHttp();
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://custom-react-hooks-e706f-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+    sendRequest(
+      {
+        url: "https://custom-react-hooks-e706f-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      sendTask.bind(null, taskText)
+    );
   };
 
   return (
